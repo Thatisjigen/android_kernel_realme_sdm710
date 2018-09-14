70c73ed2f2c8ce1d08974617ae1ebf2fd7d2d938
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-09-14 13:59:59 +0530

    sched: Don't fail isolation request for an already isolated CPU
    When isolating a CPU, a check is performed to see if there is
    only 1 active CPU in the system. If that is the case, the
    CPU is not isolated. However this check is done before testing
    if the requested CPU is already isolated or not. If the
    requested CPU is already isolated, there is no need to fail
    the isolation even when there is only 1 active CPU in the system.
    
    For example, 0-6 CPUs are isolated on a 8 CPU machine. When
    an isolation request comes for CPU6, which is already isolated,
    the current code fail the requesting thinking we end up
    with no active CPU in the system.
    
    Change-Id: I28fea4ff67ffed82465e5cfa785414069e4a180a
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6843,12 +6843,6 @@ int sched_isolate_cpu(int cpu)
 
 	cpumask_andnot(&avail_cpus, cpu_online_mask, cpu_isolated_mask);
 
-	/* We cannot isolate ALL cpus in the system */
-	if (cpumask_weight(&avail_cpus) == 1) {
-		ret_code = -EINVAL;
-		goto out;
-	}
-
 	if (!cpu_online(cpu)) {
 		ret_code = -EINVAL;
 		goto out;
@@ -6856,6 +6850,13 @@ int sched_isolate_cpu(int cpu)
 
 	if (++cpu_isolation_vote[cpu] > 1)
 		goto out;
+
+	/* We cannot isolate ALL cpus in the system */
+	if (cpumask_weight(&avail_cpus) == 1) {
+		--cpu_isolation_vote[cpu];
+		ret_code = -EINVAL;
+		goto out;
+	}
 
 	/*
 	 * There is a race between watchdog being enabled by hotplug and
