kerneltoast/android_kernel_google_bluecross

cpu_input_boost: Allow configuration of each cluster's max-boost freq
On certain CPUs, the highest frequency could be very inefficient, and
thus frequently boosting to the frequency for max boosts can incur a
high power cost. To mitigate this, allow configuration of the max-boost
frequency for each CPU cluster, so that a more efficient frequency can
be selected for max boosts.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

drivers/cpufreq/cpu_input_boost.c
@@ -35,12 +35,28 @@ struct boost_drv {
 
 static struct boost_drv *boost_drv_g __read_mostly;
 
-static u32 get_boost_freq(struct boost_drv *b, u32 cpu)
+static u32 get_input_boost_freq(struct cpufreq_policy *policy)
 {
-	if (cpumask_test_cpu(cpu, cpu_lp_mask))
-		return CONFIG_INPUT_BOOST_FREQ_LP;
+	u32 freq;
 
-	return CONFIG_INPUT_BOOST_FREQ_PERF;
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+		freq = CONFIG_INPUT_BOOST_FREQ_LP;
+	else
+		freq = CONFIG_INPUT_BOOST_FREQ_PERF;
+
+	return min(freq, policy->max);
+}
+
+static u32 get_max_boost_freq(struct cpufreq_policy *policy)
+{
+	u32 freq;
+
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+		freq = CONFIG_MAX_BOOST_FREQ_LP;
+	else
+		freq = CONFIG_MAX_BOOST_FREQ_PERF;
+
+	return min(freq, policy->max);
 }
 
 static u32 get_boost_state(struct boost_drv *b)
@@ -173,7 +189,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 {
 	struct boost_drv *b = container_of(nb, typeof(*b), cpu_notif);
 	struct cpufreq_policy *policy = data;
-	u32 boost_freq, state;
+	u32 state;
 
 	if (action != CPUFREQ_ADJUST)
 		return NOTIFY_OK;
@@ -188,7 +204,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 
 	/* Boost CPU to max frequency for max boost */
 	if (state & MAX_BOOST) {
-		policy->min = policy->max;
+		policy->min = get_max_boost_freq(policy);
 		return NOTIFY_OK;
 	}
 
@@ -196,12 +212,10 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 	 * Boost to policy->max if the boost frequency is higher. When
 	 * unboosting, set policy->min to the absolute min freq for the CPU.
 	 */
-	if (state & INPUT_BOOST) {
-		boost_freq = get_boost_freq(b, policy->cpu);
-		policy->min = min(policy->max, boost_freq);
-	} else {
+	if (state & INPUT_BOOST)
+		policy->min = get_input_boost_freq(policy);
+	else
 		policy->min = policy->cpuinfo.min_freq;
-	}
 
 	return NOTIFY_OK;
 }

Thatisjigen/android_kernel_realme_sdm710

Signed-off-by: Danny Lin <danny@kdrag0n.dev>
Signed-off-by: laststandrighthere <laststandrighthere@gmail.com>
Signed-off-by: Lau <laststandrighthere@gmail.com>
Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
Signed-off-by: Poebe S.A <poebesaddam@qmail.id>

drivers/cpufreq/cpu_input_boost.c
@@ -35,6 +35,24 @@ struct boost_drv {
 
 static struct boost_drv *boost_drv_g __read_mostly;
 
+static u32 get_input_boost_freq(struct cpufreq_policy *policy)
+{
+	u32 freq;
+
+	freq = CONFIG_INPUT_BOOST_FREQ;
+
+	return min(freq, policy->max);
+}
+
+static u32 get_max_boost_freq(struct cpufreq_policy *policy)
+{
+	u32 freq;
+
+	freq = CONFIG_MAX_BOOST_FREQ;
+
+	return min(freq, policy->max);
+}
+
 static u32 get_boost_state(struct boost_drv *b)
 {
 	return atomic_read(&b->state);
@@ -178,7 +196,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 
 	/* Boost CPU to max frequency for max boost */
 	if (state & MAX_BOOST) {
-		policy->min = policy->max;
+		policy->min = get_max_boost_freq(policy);
 		return NOTIFY_OK;
 	}
 
@@ -186,11 +204,10 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 	 * Boost to policy->max if the boost frequency is higher. When
 	 * unboosting, set policy->min to the absolute min freq for the CPU.
 	 */
-	if (state & INPUT_BOOST) {
-		policy->min = min(policy->max, CONFIG_INPUT_BOOST_FREQ);
-	} else {
+	if (state & INPUT_BOOST)
+		policy->min = get_input_boost_freq(policy);
+	else
 		policy->min = policy->cpuinfo.min_freq;
-	}
 
 	return NOTIFY_OK;
 }